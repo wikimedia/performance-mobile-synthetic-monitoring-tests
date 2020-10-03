19f04c27e9fa3078b0ec75cfa2fd905fe3e4349c
Peter Hedenskog <phedenskog@wikimedia.org>
2020-10-03 16:59:21 +0200

    Add outer loop for the start script.
    * This helps us with two things: we can change the script that runs the
    actual tests and it will be picked up on the server.
    * When we rotate the logs, the next source (when we start the tests)
    they will log to our log file.
    
    Bug: T263907
    Change-Id: Ie8758b7fbe34e2baae6a72ad18bf6cdd2e9046d4

---
+++ runTheTests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+LOGFILE=./logs/$1.log
+exec > $LOGFILE 2>&1
+DEVICE_ID=${1%%-*}
+
+for fileAndPath in tests/$1/*.txt ; do
+    [ -e "$fileAndPath" ] || continue
+    FILENAME=$(basename $fileAndPath)
+    POTENTIAL_CONFIG="./config/$1.${FILENAME%%.*}.json"
+    [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$POTENTIAL_CONFIG" || CONFIG_FILE="./config/$1.json"
+    sitespeed.io --config ./$CONFIG_FILE $fileAndPath
+    control
+    sleep 120
+done
+for scriptAndPath in tests/$1/*.js ; do
+    [ -e "$scriptAndPath" ] || continue
+    FILENAME=$(basename $scriptAndPath)
+    POTENTIAL_CONFIG="./config/$1.${FILENAME%%.*}.json"
+    [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$POTENTIAL_CONFIG" || CONFIG_FILE="./config/$1.json"
+    sitespeed.io --config ./$CONFIG_FILE $scriptAndPath --multi
+    control
+    sleep 120
+done
+# At the moment we only run WebPageReplay on one of the phones
+if [ "$DEVICE_ID" = "ZY3222N2CZ" ]; then
+    for file in tests/$1/*.wpr ; do
+        while IFS= read url
+        do
+            DEVICE_SERIAL=$DEVICE_ID ./wpr/wprAndroid.sh $url --config ./config/$1.json
+            control
+            sleep 180
+        done <"$file"
+        control
+        sleep 500
+    done
+fi
--- start.sh
+++ start.sh
@@ -18,11 +18,6 @@ else
     touch $CONTROL_FILE
 fi
 
-LOGFILE=./logs/$1.log
-exec > $LOGFILE 2>&1
-
-DEVICE_ID=${1%%-*}
-
 # Help us end on demand
 function control() {
     if [ -f "$CONTROL_FILE" ]
@@ -31,7 +26,7 @@ function control() {
     else
         echo "$CONTROL_FILE not found - stopping after cleaning up ..."
         echo "Exit"
-        exit 0
+        exit 1
     fi
 }
 
@@ -39,35 +34,12 @@ while true
 do
     ## For each iteration, we pull the latest code from git and run
     git pull
-    for fileAndPath in tests/$1/*.txt ; do
-        [ -e "$fileAndPath" ] || continue
-        FILENAME=$(basename $fileAndPath)
-        POTENTIAL_CONFIG="./config/$1.${FILENAME%%.*}.json"
-        [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$POTENTIAL_CONFIG" || CONFIG_FILE="./config/$1.json"
-        sitespeed.io --config ./$CONFIG_FILE $fileAndPath
-        control
-        sleep 120
+    do
+      source runTheTests.sh "$@"
+      result=$?
+      if [ $result -ne 0 ]; then
+          echo 'Stop the loop $result' 
+          exit 0;
+      fi
     done
-    for scriptAndPath in tests/$1/*.js ; do
-        [ -e "$scriptAndPath" ] || continue
-        FILENAME=$(basename $scriptAndPath)
-        POTENTIAL_CONFIG="./config/$1.${FILENAME%%.*}.json"
-        [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$POTENTIAL_CONFIG" || CONFIG_FILE="./config/$1.json"
-        sitespeed.io --config ./$CONFIG_FILE $scriptAndPath --multi
-        control
-        sleep 120
-    done
-    # At the moment we only run WebPageReplay on one of the phones
-    if [ "$DEVICE_ID" = "ZY3222N2CZ" ]; then
-        for file in tests/$1/*.wpr ; do
-            while IFS= read url
-            do
-                DEVICE_SERIAL=$DEVICE_ID ./wpr/wprAndroid.sh $url --config ./config/$1.json
-                control
-                sleep 180
-            done <"$file"
-            control
-            sleep 500
-        done
-    fi
 done
