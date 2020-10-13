4a0e9f7075f36e45fc38bb243fee022335e83c64
Peter Hedenskog <phedenskog@wikimedia.org>
2020-10-13 12:39:22 +0200

    Cleanup start script: start one or all phones.
    Change-Id: I1d8282b88f01470863e80be45728907f975ac2bd

--- start.sh
+++ start.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-# The first parameter is the phone name, so we can find the right tests to run
-if [ -d "config/$1" ]
-then
-    echo "Missing input/config directory. You need to run with a parameter that gives the path to the configuration for the phone. "
-    exit 1
-fi
+PHONES=(ZY3222N2CZ-Moto-G5-Root-WebPageReplay ZY2242HHQ8-Moto-G4 ZY2242HJDX-Moto-G4)
 
-CONTROL_FILE="./locks/$1.run"
-
-# You cannot start multiple instances!
-if [ -f "$CONTROL_FILE" ]
+if [ -z "$1" ]
 then
-    echo "$CONTROL_FILE exist, do you have running tests?"
-    exit 1;
+    for phone in  "${PHONES[@]}"; do
+        CONTROL_FILE="./locks/$phone.run"
+        
+        if [ -f "$CONTROL_FILE" ]
+        then
+            echo "Not starting $phone since lock file $CONTROL_FILE exist."
+        else
+            echo "Starting $phone"
+            nohup ./loop.sh $phone &
+        fi
+    done
 else
-    touch $CONTROL_FILE
-fi
-
-# Help us end on demand
-function control() {
+    CONTROL_FILE="./locks/$1.run"
+    
     if [ -f "$CONTROL_FILE" ]
     then
-        echo "$CONTROL_FILE found. Make another run ..."
+        echo "Not starting $1 since lock file $CONTROL_FILE exist."
     else
-        echo "$CONTROL_FILE not found - stopping after cleaning up ..."
-        echo "Exit"
-        exit 1
+        echo "Starting $1"
+        nohup ./loop.sh $1 &
     fi
-}
-
-while true
-do
-    ## For each iteration, we pull the latest code from git and run
-    git pull
-    source runTheTests.sh "$@"
-    result=$?
-    if [ $result -ne 0 ]; then
-        echo 'Stop the loop $result'
-        exit 0;
-    fi
-done
+fi
