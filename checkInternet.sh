55a5c7cea131016bf71ada1d5c35cce0af0512ee
Peter Hedenskog <phedenskog@wikimedia.org>
2020-12-06 14:53:12 +0100

    Also print model and temperature.
    Change-Id: I0a78cae8c858d185ec78692e6bbc1888fbcc9657

--- checkInternet.sh
+++ checkInternet.sh
@@ -2,9 +2,11 @@
 
 for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
 do
+    MODEL=$(adb -s $DEVICE shell getprop ro.product.model)
+    TEMP=$(adb -s $DEVICE shell dumpsys battery | grep temperature | grep -Eo '[0-9]{1,3}')
     if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
-        echo $DEVICE has a connection to the internet $state
+        echo $DEVICE $MODEL has a connection to the internet $state and the battery  temperature $((TEMP / 10)) degrees
     else
-        echo $DEVICE has no connection to the internet $state
+        echo $DEVICE $MODEL has no connection to the internet $state and the battery temperature $((TEMP / 10)) degrees
     fi
 done
