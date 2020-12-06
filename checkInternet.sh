d8b3210ebf2f0571086a76e8c734a657036836de
Peter Hedenskog <phedenskog@wikimedia.org>
2020-12-06 15:08:20 +0100

    Check the state of the phone before continuing.
    Bug: T269526
    Change-Id: I689afe39e416cdecc0fe6dfb24f85f38af400fdd

--- checkInternet.sh
+++ checkInternet.sh
@@ -2,11 +2,16 @@
 
 for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
 do
-    MODEL=$(adb -s $DEVICE shell getprop ro.product.model)
-    TEMP=$(adb -s $DEVICE shell dumpsys battery | grep temperature | grep -Eo '[0-9]{1,3}')
-    if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
-        echo $DEVICE $MODEL has a connection to the internet $state and the battery  temperature $((TEMP / 10)) degrees
+    STATE=$(adb -s $DEVICE get-state)
+    if [ "$STATE" = "device" ]; then
+        MODEL=$(adb -s $DEVICE shell getprop ro.product.model)
+        TEMP=$(adb -s $DEVICE shell dumpsys battery | grep temperature | grep -Eo '[0-9]{1,3}')
+        if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
+            echo $DEVICE $MODEL has a connection to the internet $state and the battery  temperature $((TEMP / 10)) degrees
+        else
+            echo $DEVICE $MODEL has no connection to the internet $state and the battery temperature $((TEMP / 10)) degrees
+        fi
     else
-        echo $DEVICE $MODEL has no connection to the internet $state and the battery temperature $((TEMP / 10)) degrees
+        echo $DEVICE is $STATE
     fi
 done
