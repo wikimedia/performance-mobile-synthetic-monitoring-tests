93da2a993163aad7751c06156edaaa97b22ddb63
Peter Hedenskog <phedenskog@wikimedia.org>
2020-12-07 09:03:41 +0100

    Rename the check script since do more things than check internet connection.
    Change-Id: I488dc4a61150154c45d8b76a1aee0c5c007826c4

--- checkPhones.sh
+++ checkPhones.sh
@@ -7,11 +7,11 @@ do
         MODEL=$(adb -s $DEVICE shell getprop ro.product.model)
         TEMP=$(adb -s $DEVICE shell dumpsys battery | grep temperature | grep -Eo '[0-9]{1,3}')
         if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
-            echo $DEVICE $MODEL has a connection to the internet $state and the battery  temperature $((TEMP / 10)) degrees
+            echo $DEVICE $MODEL has a connection to the internet $state and the battery  temperature is $((TEMP / 10)) degrees
         else
-            echo $DEVICE $MODEL has no connection to the internet $state and the battery temperature $((TEMP / 10)) degrees
+            echo $DEVICE $MODEL has no connection to the internet $state and the battery temperature is $((TEMP / 10)) degrees
         fi
     else
-        echo $DEVICE is $STATE
+        echo $DEVICE state is $STATE
     fi
 done
