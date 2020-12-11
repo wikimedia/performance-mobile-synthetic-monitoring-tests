e8499bd3b33a3113e677d158fb3609063e4e209d
Peter Hedenskog <phedenskog@wikimedia.org>
2020-12-11 09:21:40 +0100

    Show connected wifi name.
    Bug: T269917
    Change-Id: If0ab4d51a99eb6146212dc41ed7f0a926c5b0e52

--- checkPhones.sh
+++ checkPhones.sh
@@ -7,9 +7,10 @@ do
         MODEL=$(adb -s $DEVICE shell getprop ro.product.model)
         TEMP=$(adb -s $DEVICE shell dumpsys battery | grep temperature | grep -Eo '[0-9]{1,3}')
         if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
-            echo $DEVICE $MODEL has a connection to the internet $state and the battery  temperature is $((TEMP / 10)) degrees
+            WIFI=$(adb -s $DEVICE shell dumpsys netstats | grep -E 'iface=wlan.*networkId'  | awk '{print $4}' | head -1 | cut -d'"' -f 2)
+            echo $DEVICE $MODEL has a connection to the internet wifi $WIFI and the battery  temperature is $((TEMP / 10)) degrees
         else
-            echo $DEVICE $MODEL has no connection to the internet $state and the battery temperature is $((TEMP / 10)) degrees
+            echo $DEVICE $MODEL has no connection to the internet and the battery temperature is $((TEMP / 10)) degrees
         fi
     else
         echo $DEVICE state is $STATE
