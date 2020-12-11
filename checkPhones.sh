245a6fad51b52dc03b38daf86a9039739f8ed875
Peter Hedenskog <phedenskog@wikimedia.org>
2020-12-11 15:23:55 +0100

    Add exception for checking wifi for the WebPageReplay phone.
    Change-Id: Ifa41deac3c7dac1738568450e5da970f061aebf4

--- checkPhones.sh
+++ checkPhones.sh
@@ -6,11 +6,15 @@ do
     if [ "$STATE" = "device" ]; then
         MODEL=$(adb -s $DEVICE shell getprop ro.product.model)
         TEMP=$(adb -s $DEVICE shell dumpsys battery | grep temperature | grep -Eo '[0-9]{1,3}')
-        if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
-            WIFI=$(adb -s $DEVICE shell dumpsys netstats | grep -E 'iface=wlan.*networkId'  | awk '{print $4}' | head -1 | cut -d'"' -f 2)
-            echo $DEVICE $MODEL has a connection to the internet wifi $WIFI and the battery  temperature is $((TEMP / 10)) degrees
+        if [ "$DEVICE" = "ZY3222N2CZ" ]; then
+            echo $DEVICE $MODEL uses the internet connection from the Mac Mini together with WebPageReplay and the battery temperature is $((TEMP / 10)) degrees
         else
-            echo $DEVICE $MODEL has no connection to the internet and the battery temperature is $((TEMP / 10)) degrees
+            if adb -s $DEVICE shell ping -c 1 8.8.8.8 | grep -q rtt; then
+                WIFI=$(adb -s $DEVICE shell dumpsys netstats | grep -E 'iface=wlan.*networkId'  | awk '{print $4}' | head -1 | cut -d'"' -f 2)
+                echo $DEVICE $MODEL has a connection to the internet wifi $WIFI and the battery temperature is $((TEMP / 10)) degrees
+            else
+                echo $DEVICE $MODEL has no connection to the internet and the battery temperature is $((TEMP / 10)) degrees
+            fi
         fi
     else
         echo $DEVICE state is $STATE
