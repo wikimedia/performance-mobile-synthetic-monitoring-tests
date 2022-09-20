70b578c13e4b887ceb6a8c8b0434c26dc0271644
Peter Hedenskog <phedenskog@wikimedia.org>
2022-09-20 16:27:11 +0200

    Add second view tests.
    Change-Id: Iae5fbd66fe36c504a4d2ad032e089798d2b743ea

---
+++ tests/secondView.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+URLS=('https://en.m.wikipedia.org/wiki/Barack_Obama' 'https://en.m.wikipedia.org/wiki/Facebook' 'https://en.m.wikipedia.org/wiki/Sweden')
+
+for url in ${URLS[@]}; do
+    sitespeed.io $url --config ./config/secondView.json
+    adb -shell am force-stop "com.android.chrome"
+    adb -shell pm clear "com.android.chrome"
+    sleep 10
+done
