589e438980365936b207076e00de8600797ae29d
Peter Hedenskog <phedenskog@wikimedia.org>
2022-10-16 19:27:06 +0200

    Test four URLs with Firefox.
    Bug: T318415
    Change-Id: I4cda33b90f43a91fecdada50a2266b38f0bdcbad

--- tests/firstView.sh
+++ tests/firstView.sh
@@ -14,7 +14,7 @@ done
 curl -L https://github.com/mozilla-mobile/fenix/releases/download/v105.2.0/fenix-105.2.0-armeabi-v7a.apk --output firefox.apk
 adb install  -r firefox.apk
 
-FIREFOX_URLS=('https://en.m.wikipedia.org/wiki/Barack_Obama' 'https://en.m.wikipedia.org/wiki/Main_Page')
+FIREFOX_URLS=('https://en.m.wikipedia.org/wiki/Barack_Obama' 'https://en.m.wikipedia.org/wiki/Main_Page' 'https://en.m.wikipedia.org/wiki/Facebook' 'https://en.m.wikipedia.org/wiki/Sweden')
 
 for url in ${FIREFOX_URLS[@]}; do
     sitespeed.io $url --config ./config/firstViewFirefox.json -b firefox
