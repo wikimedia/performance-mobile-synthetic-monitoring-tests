e10efbf44e2736c15436fbc5701c0d18f6903029
Peter Hedenskog <phedenskog@wikimedia.org>
2022-11-03 08:41:23 +0100

    node 16
    Change-Id: I0eead0f6de90b25b33faabd0ea3ca7d18ffd40aa

--- install/linux/nodejs.sh
+++ install/linux/nodejs.sh
@@ -2,6 +2,6 @@
 
 # Install NodeJs LTS
 apt install sudo
-curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 apt install -y nodejs
