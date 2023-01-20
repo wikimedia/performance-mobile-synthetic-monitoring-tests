0cda1c3b27a85dd20421fbc2ce1859bd90b356b6
Peter Hedenskog <phedenskog@wikimedia.org>
2023-01-20 09:45:34 +0100

    Make sure to install sudo.
    Bug: T327472
    Change-Id: I5b298de5665787c57d2ededc33dbbf8c1530cbee

--- install/linux/nodejs.sh
+++ install/linux/nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install NodeJs LTS
-apt install sudo
+apt install -y sudo
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 apt install -y nodejs
