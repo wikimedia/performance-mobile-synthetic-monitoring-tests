dea135b3ea16a42702cb89aa7916381dfb01d5c7
Peter Hedenskog <phedenskog@wikimedia.org>
2023-03-15 09:07:19 +0100

    Install Firefox before we disable the wifi.
    It makes more sense to install Firefox before we disable the wifi
    and traffic are sent back to the server.
    
    Change-Id: If0273b6087a959901ccdded521515dc9b161b533

--- tests/webpagereplay.sh
+++ tests/webpagereplay.sh
@@ -4,6 +4,9 @@ curl -L "https://github.com/sitespeedio/replay/blob/main/replay/linux/wpr?raw=tr
 chmod 755 wpr
 mv wpr ./webpagereplay/wpr
 
+curl -L https://github.com/mozilla-mobile/firefox-android/releases/download/fenix-v111.0/fenix-111.0-arm64-v8a.apk --output firefox.apk
+adb install  -r firefox.apk
+
 # Disable the wifi on the device to make sure we are recording/replaying through the server
 adb shell svc wifi disable
 
@@ -17,11 +20,7 @@ for url in ${URLS[@]}; do
     sleep 120
 done
 
-curl -L https://github.com/mozilla-mobile/firefox-android/releases/download/fenix-v111.0/fenix-111.0-arm64-v8a.apk --output firefox.apk
-adb install  -r firefox.apk
-
 FIREFOX_URLS=('https://en.m.wikipedia.org/wiki/Barack_Obama' 'https://en.m.wikipedia.org/wiki/Main_Page' 'https://en.m.wikipedia.org/wiki/Facebook' 'https://en.m.wikipedia.org/wiki/Sweden')
-
 
 for url in ${FIREFOX_URLS[@]}; do
     ANDROID=true ./webpagereplay/replay.sh --config ./config/replay.json $url -b firefox --browsertime.iterations 5
