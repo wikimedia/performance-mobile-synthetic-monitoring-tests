81736d832212227de8298f026cca46d0bebc3bed
Peter Hedenskog <phedenskog@wikimedia.org>
2023-04-05 18:26:08 +0200

    Better handling of of input.
    Bug: T330402
    Change-Id: Ibe3a60c8a067288c8a9769a56217bb8ce9a735cf

--- start.sh
+++ start.sh
@@ -8,10 +8,13 @@
 ./install/linux/nodejs.sh
 ./install/linux/sitespeed.io.sh
 
+echo "We got $1 and $2 as input"
+
 # We use a hack to pass on parameters
 # CALABASH_TAGS and CALABASH_PROFILE
 # and we sent the type and browser in one
-TYPE_OF_TEST_AND_BROWSER=(${1//;/ })
+INPUT=$1
+TYPE_OF_TEST_AND_BROWSER=(${INPUT//;/ })
 TYPE_OF_TEST=${TYPE_OF_TEST_AND_BROWSER[0]}
 BROWSER=${TYPE_OF_TEST_AND_BROWSER[1]}
 FILE_TO_RUN=$2
