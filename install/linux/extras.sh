1daff43c046e5783aa855547093214e3386510c2
Peter Hedenskog <phedenskog@wikimedia.org>
2023-04-13 12:29:41 +0200

    Export the Python that got the right dependencies.
    Change-Id: Iecb63fbeacc4a8e1eae1b9ec558e18e4a6653deb

--- install/linux/extras.sh
+++ install/linux/extras.sh
@@ -2,3 +2,6 @@
 
 # All installations are now done in the Docker container
 # But if need something quick, we can add that in this file
+
+# Exporting the Python that got the dependencies
+export PYTHON=/usr/local/bin/python3
