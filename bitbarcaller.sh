41b5a50738d159c52a69744333f4830c4c391366
Peter Hedenskog <phedenskog@wikimedia.org>
2023-04-14 08:25:48 +0200

    Update comments in the code.
    Change-Id: I100bebde1bf717d27b34cbf4cf83620b667459ee

--- bitbarcaller.sh
+++ bitbarcaller.sh
@@ -13,12 +13,29 @@ fi
 
 # These are settings used by BitBar
 # You can see these in BitBar GUI
+
+# The frameworks is Appium and 
+# special Appium with NET_ADMIN rights in Docker
+# We do not use Appium though.
 WEBPAGEREPLAY_FRAMEWORK_ID=19
-WEBPAGEREPLAY_PROJECT_ID=204
-SAMSUNG_DEVICE_GROUP_ID=10
 DIRECT_FRAMEWORK_ID=7
+
+# The project is where all test live
+# You can look in each project in the GUI and see latest tests
+# per project
+WEBPAGEREPLAY_PROJECT_ID=204
 DIRECT_PROJECT_ID=257
+
+# The phones are grouped so in each group only one phone
+# take on the job. We have one group of Samsung phones and
+# one with Moto G5 phones
+SAMSUNG_DEVICE_GROUP_ID=10
 MOTO_DEVICE_GROUP_ID=7
+
+# To be able to run tests on BitBar you need to supply a APK 
+# file. In our case we just use a sample app.
+# The RUN_TEST_FILE is the file that is executed first in the tes
+# we upload that to the GUI.
 APK_FILE_ID=15
 RUN_TEST_FILE_ID=62535
 
