25a52a31793a9b62c159450d56418e78e76d1cbe
Peter Hedenskog <phedenskog@wikimedia.org>
2023-06-28 09:49:23 +0200

    Remove unused tests and add second view tests for vi.
    Bug: T340608
    Change-Id: Ia6752a6c76252904b09ce78af2353e425e1c0b41

--- tests/direct/clientPreferencesBefore.cjs
+++
@@ -1,8 +0,0 @@
-const factory = require( './includes/clientPreferencesFactory.cjs' );
-
-module.exports = factory(
-	'Test before multiple client preferences',
-	'Baseline test for client preferences',
-	"/speed-tests/United_States.mobile.enwiki.1148217368/before/",
-	async () => {}
-);
